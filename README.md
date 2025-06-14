df69ccb92842cb08c23c50b5fbe95188c8404ef0
hannahilea <hannahilea@users.noreply.github.com>
2025-06-14 16:37:45 -0400

    update site builder for this site

--- README.md
+++ README.md
@@ -20,9 +20,9 @@ Mapping the current state of nut-allergy-friendliness of various restaurants, co
 - [x] change visibility of site to public
 - [x] set up github pages for site (not possible until public)
 - [x] embed map in static site
+- [x] generate static site from script
 - [ ] add license
-- [ ] script skeleton (manual)
-- [ ] generate static site from script
+- [ ] end-to-end script skeleton (manual)
 - [ ] fill in script:
   - [ ] download sheet csv 
   - [ ] parse entries for website
@@ -31,18 +31,22 @@ Mapping the current state of nut-allergy-friendliness of various restaurants, co
   - [ ] format entries as map format (KML??)
 - [ ] figure out map upload; may be manual to start?
 - [ ] add big ol' faq/disclaimer about role of this info
+- [ ] add links to tools used (e.g. css, google maps, etc)
 
 
 ### Feature creep future:
 - Run script via GHA instead of manually
 - Add filtering to table
 - Multiple tables for various restaurant categories (safety, type, stars, etc)
-- Handle different geographies (filters/pages/whatever)
+- Handle different geographies (filters/pages/whatever); Add location to table (city or zipcode etc)?
 - Template for contacting restaurants
 - Figure out privacy issues of delayed updating of site, private version of site while traveling, etc
 - Support multiple entries for the same venue (e.g. timestamped dates for updated menus, updated queries, etc)
 - "hall of fame" for places that actively promote allergen friendliness AND that have moved from non-friendly to friendly
 - Add (separate) workflow that supports nicer realtime site investigation (interop with Google Maps directly, etc)
+- Figure out how to add tags for venue type (cafe/food/etc)
+- Add separate page (?) for chain restaurants?
+
 
 ## Fields for form 
 - date contacted
@@ -56,3 +60,6 @@ Mapping the current state of nut-allergy-friendliness of various restaurants, co
 
 optional:
 - link to menu
+
+## Credits
+- Thanks to [wescpy.blogspot.com](https://wescpy.blogspot.com/2016/07/exporting-google-sheet--as-csv.html) for the pointer to use the Google Drive API instead of the Google Sheets API
--- site-builder/run.jl
+++ site-builder/run.jl
@@ -1,281 +1,70 @@
 using Pkg
 Pkg.activate(@__DIR__)
-using pandoc_jll
 using Dates
-using YAML
+using CSV
+using DataFrames
 
-const BLOG_DIR = joinpath(@__DIR__, "..", "blog")
-const BLOG_TEMPLATE = joinpath(BLOG_DIR, "__template", "blog.template.html")
-const BLOG_INDEX_TEMPLATE = joinpath(BLOG_DIR, "__template", "index.template.html")
-const BLOG_FOOTER = read(joinpath(BLOG_DIR, "__template", "blog-footer.html"), String)
+const SITE_DIR = joinpath(@__DIR__, "..")
+const SITE_INDEX_TEMPLATE = joinpath(@__DIR__, "index.template.html")
 
-const PROJECT_DIR = joinpath(@__DIR__, "..", "projects")
-const PROJECT_INDEX_TEMPLATE = joinpath(PROJECT_DIR, "__template", "index.template.html")
+function get_table_html(df)
+    str = """
+          <table>
+                    <thead>
+                        <tr>
+                        {{ TABLE_HEAD }}
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {{ TABLE_ROWS }}
+                    </tbody>
+                </table>
+                """
 
-const RSS_FILE = joinpath(@__DIR__, "..", "rss.xml")
-const RSS_TEMPLATE = joinpath(BLOG_DIR, "__template", "rss.template.xml")
+    # Table head...
+    rows_str = join(["<td>" * v * "</td>\n" for v in names(df)], "")
+    str = replace(str, "{{ TABLE_HEAD }}" => rows_str)
 
-get_warning(file)  = "<!--Auto-generated! Make changes in $(basename(file)), not this file-->\n"
-
-function convert_to_html(file, outfile; template=BLOG_TEMPLATE, overwrite_existing=false)
-    if !overwrite_existing && isfile(outfile)
-        @warn "Output file already exists; not overwriting: $outfile"
-        return nothing
+    # Table body...
+    rows_str = map(eachrow(df)) do entry
+        row_str = ["<td>" * v * "</td>\n" for v in values(entry)]
+        replace!(row_str, missing => "<td></td>\n")
+        return "<tr>\n" * join(row_str, "") * "</tr>\n"
     end
-    cmd = pipeline(`$(pandoc_jll.pandoc()) --template $template -o $outfile --highlight-style tango $file `)
-    @debug "About to run pandoc" cmd
-    run(pipeline(cmd))
-
-    # Get date and format it 
-    date_pretty = let
-        yaml_dict = YAML.load_file(file)
-        str = string(yaml_dict["created"])
-        Dates.format(Date(str), dateformat"d u yyyy")
-    end
-
-    # For now, do a very brittle tuning of properties!
-    # In future, turn this into a pandoc plugin 
-    str = read(outfile, String)
-    str = replace(str, "{{ BLOG_DIR }}" => basename(dirname(file)))
-    str = replace(str, "{{ BLOG_DATE }}" => date_pretty)
-    str = replace(str, "{{ BLOG_FOOTER }}" => BLOG_FOOTER)
-    str = tweak_html!!(str)
-    write(outfile, str)
-
-    return nothing
+    return str = replace(str, "{{ TABLE_ROWS }}" => join(rows_str, "\n"))
 end
 
-function tweak_html!!(text)
-    # Checkboxes are default enabled---disable them (https://github.com/jgm/pandoc/issues/8562)
-    text = replace(text, " type=\"checkbox\"" => " disabled type=\"checkbox\"")
-
-    # Add footnotes heading (this is SO GROSS and we're doing it anyway)
-    fnote_predecessor = "role=\"doc-endnotes\">\n<hr />"
-    fnote_heading = """\n<h3 id="footnotes-title">Footnotes</h3>"""
-    text = replace(text, fnote_predecessor => fnote_predecessor * fnote_heading)
-
-    # the formatter will yell otherwise...
-    text = replace(text, "</br>" => "<br>")
-
-    # Strip out image captions 
-    # This is...a truly hacky approach. ¯\_(ツ)_/¯
-    in_caption = false
-    str_start = "<figcaption aria-hidden=\"true\">"
-    str_stop = "</figcaption>"
-    lines = map(split(text, "\n")) do line
-        # Making some real assumptions that there'll never be multiple of these in one line
-        # ...but if there are, we'll see it in git, so it'll still be okay
-
-        # Single line contains both start and stop
-        if !in_caption && contains(line, str_start) && contains(line, str_stop)
-            line = split(line, str_start; limit=2)[1]
-            return split(line, str_stop; limit=2)[end]
-        end
-
-        # Multiline caption
-        if in_caption && contains(line, str_stop)
-            in_caption = false
-            return split(line, str_stop; limit=2)[end]
-        end
-        in_caption && return missing
-        if contains(line, str_start)
-            in_caption = true
-            line = split(line, str_start; limit=2)[1]
-        end
-        return line
-    end
-    lines = filter(!ismissing, lines)
-    all_text = join(lines, "\n")
-
-    # linking to an external page (i.e., not within the site)? open in a new tab 
-    words = split(all_text, " ")
-    phrases = map(words) do word 
-        contains(word, "href=") || return word
-        contains(word, "href=\"#") && return word
-        contains(word, "href=\"/") && return word
-        startswith(word, "href=\"https://www.hannahilea.com") && return word
-        contains(word, "href=\".") && return word
-        contains(word, "href=\"{") && return word
-        startswith(word, "raw_href=") && return word[5:end]
-        str = "target=\"_blank\" rel=\"noreferrer noopener\""
-        return replace(word, "href="=> "$str href=")
-    end
-    return join(phrases, " ")
+function wrap_in_details_block(summary, content)
+    return """
+           <details><summary>$summary</summary>
+               $content
+           </details>
+           """
 end
 
-function generate_blog_html(md_file; overwrite_existing=true)
-    if !isfile(md_file)
-        @warn "Expected blog source file not found: `$(md_file)`; skipping"
-        return nothing
-    end
 
-    @info "Converting $(basename(dirname(md_file)))..."
-    html_outfile = replace(md_file, "src.md" => "index.html")
-    convert_to_html(md_file, html_outfile; overwrite_existing)
-
-    @info "...and formatting it"
-    try
-        run(`prettier $(html_outfile) --write --print-width 240`)
-    catch
-        @warn "Prettier not installed OR current html errors"
-    end
-    return nothing
-end
-
-function generate_all_blogposts(; overwrite_existing=true)
-    for dir in readdir(BLOG_DIR; join=true)
-        isfile(dir) && continue
-        isequal(joinpath(BLOG_DIR, "__template"), dir) && continue
-        # contains(dir, "list") || continue
-
-        md_file = joinpath(dir, "src.md")
-        isfile(md_file) || continue
-        generate_blog_html(md_file; overwrite_existing)
-    end
-    return nothing
-end
-
-function get_blog_metadata(md_file) 
-    yaml_dict = YAML.load_file(md_file)
-    yaml_dict["tags"] = join(yaml_dict["tags"], ",")
-    yaml_dict["date_str"] = string(yaml_dict["created"])
-    yaml_dict["rawtitle"] = haskey(yaml_dict, "rawtitle") ? yaml_dict["rawtitle"] : yaml_dict["title"]
-    return NamedTuple(Symbol(k) => v for (k,v) in yaml_dict)
-end
-
-function generate_blog_index(; overwrite_existing=false, template=BLOG_INDEX_TEMPLATE)
-    outfile = joinpath(BLOG_DIR, "index.html")
-    if !overwrite_existing && isfile(outfile)
-        @warn "Output file already exists; not overwriting: $outfile"
-        return nothing
-    end
-
-    @info "Generating blog index..."
-    metadata = get_all_blog_metadata()
-    blog_strs = map(metadata) do m
-        date_pretty = Dates.format(Date(m.date_str), dateformat"d u yyyy")
-        tags = "#" * replace(m.tags, "," => " #")
-
-        return """
-        <tr>
-            <td class="date date-pretty">$(date_pretty)</td>
-            <td class="date-raw" hidden>$(m.date_str)</td>
-            <td class="title-raw" hidden>$(m.title)</td>
-            <td class="title">
-                <a class="blog-url" href="$(m.url)">$(m.title)</a>
-
-            </td>
-          </tr>
-          """
-    end
-
-    str = read(template, String)
-    str = replace(str, "<!-- POSTS -->" => join(blog_strs, "\n"))
-     str = replace(str, "{{ BLOG_FOOTER }}" => BLOG_FOOTER)
-    str = get_warning(template) * str
-    write(outfile, str)
-
-    try
-        run(`prettier $(outfile) --write --print-width 360`)
-    catch
-        @warn "Prettier not installed OR current html errors"
-    end
-
-    return nothing
-end
-
-function get_all_blog_metadata()
-    metadata = []
-    for dir in readdir(BLOG_DIR; join=true)
-        isfile(dir) && continue
-        isequal(joinpath(BLOG_DIR, "__template"), dir) && continue
-
-        md_file = joinpath(dir, "src.md")
-        isfile(md_file) || continue
-        m = get_blog_metadata(md_file)
-        push!(metadata, (; url="./" * basename(dir), dir=basename(dir), m...))
-    end
-    metadata = sort(metadata; by=(m) -> m.date_str, rev=true)
-    return metadata
-end
-
-function generate_rss_feed(; overwrite_existing=false, template=RSS_TEMPLATE)
-    outfile = RSS_FILE
-    if !overwrite_existing && isfile(outfile)
-        @warn "Output file already exists; not overwriting: $outfile"
-        return nothing
-    end
-
-    @info "Generating RSS feed..."
-    metadata = get_all_blog_metadata()
-    blog_strs = map(reverse(metadata)) do m
-        return """    <item>
-                  <title>$(m.rawtitle)</title>
-                  <pubDate>$(m.published)</pubDate>
-                  <link>https://hannahilea.com/blog/$(basename(m.url))</link>
-                  <guid>https://hannahilea.com/blog/$(basename(m.url))</guid>
-                  <description>$(m.description)</description>
-                </item>
-            """
-    end
-
-    str = read(template, String)
-    str = replace(str, "<!-- POSTS -->" => join(blog_strs, ""))
-    write(outfile, str)
-    return nothing
-end
-
-function generate_project_index(; overwrite_existing=false, template=PROJECT_INDEX_TEMPLATE)
-    input_data = joinpath(PROJECT_DIR, "project-list.yaml")
-    outfile = joinpath(PROJECT_DIR, "index.html")
+function generate_index(entries_df; overwrite_existing=false, template=SITE_INDEX_TEMPLATE)
+    outfile = joinpath(SITE_DIR, "index.html")
     if !overwrite_existing && isfile(outfile)
         @warn "Output file already exists; not overwriting: $outfile"
         return nothing
     end
     index_str = read(template, String)
 
-    @info "Generating project index..."
-    all_projects = YAML.load_file(input_data)
+    data = select(entries_df,
+                  :Classification,
+                  Symbol("Name of place") => :Place,
+                  Symbol("Google maps link") => ByRow(str -> "<a href=\"$str\">[link]</a>") => :Link,
+                  :Comments => ByRow(str -> ismissing(str) ? "" : str) => :Details,
+                  Symbol("Timestamp") => ByRow(str -> Dates.format(Date(first(split(str, " ")), dateformat"m/d/yyyy"), dateformat"uu yyyy")) => Symbol("Last checked"))
 
-    is_present = x -> !(isnothing(x) || isempty(x))
-
-    for key in keys(all_projects)
-        proj_strs = map(all_projects[key]) do p
-            url = get(p, "url", "")
-            name = get(p, "project", "")
-            description = get(p, "description", "")
-            blog_str = get(p, "blogs", "")
-            tag_str = get(p, "tags", "")
-            thumbnail_url = get(p, "thumbnail_url", "")
-
-            url_prefix = is_present(url) ? "<a class=\"project-url\" href=\"$(url)\">" :
-                         "<strong>"
-            url_suffix = is_present(url) ? "</a>" : "</strong>"
-
-            thumbnail_str = is_present(thumbnail_url) ?
-                            """$(url_prefix)<img class="thumbnail" src="$(thumbnail_url)"/>$(url_suffix)""" :
-                            ""
-
-            details_str = is_present(description) ? """
-                          <p class="blog-tags">$(description) 
-                          $(tag_str)
-                          $(blog_str)</p>
-                          """ : ""
-
-            return """
-            <tr>
-                <td class="year date">$(get(p, "year", ""))</td>
-                <td class="title">
-                    $(url_prefix)$name$(url_suffix)
-                    <div class="details">
-                        $(thumbnail_str)
-                        $(details_str)
-                    </div>
-                </td>
-            </tr>
-            """
-        end
-        index_str = replace(index_str, "<!-- POSTS-$key -->" => join(proj_strs, "\n"))
+    out_str = ""
+    for gdf in groupby(data, :Classification)
+        title = first(gdf.Classification)
+        out_str = get_table_html(select(gdf, Not(:Classification)))
+        template_key = replace(uppercase(title), " " => "_")
+        index_str = replace(index_str, "{{ $template_key }}" => out_str)
+        index_str = replace(index_str, "{{ $(template_key)_COUNT }}" => nrow(gdf))
     end
     write(outfile, index_str)
 
@@ -290,19 +79,16 @@ end
 
 # Run from commandline? 
 if abspath(PROGRAM_FILE) == @__FILE__
-    if isempty(ARGS)
-        generate_all_blogposts(; overwrite_existing=true)
-        generate_blog_index(; overwrite_existing=true)
-        generate_rss_feed(; overwrite_existing=true)
-    elseif isfile(ARGS[1]) && endswith(ARGS[1], ".md")
-        generate_blog_html(ARGS[1]; overwrite_existing=true)
-        generate_blog_index(; overwrite_existing=true)
-        generate_rss_feed(; overwrite_existing=true)
-    elseif isfile(ARGS[1]) || isequal(ARGS[1], "projects")
-        generate_project_index(; overwrite_existing=true)
-    else
-        @warn "Unknown/unsupported arguments"
+    if "--download" in ARGS
+        AIzaSyDjwVcZXpD4Knn9MnrAKuipANdVRu39GAU
     end
+
+    @info "Generating index.html"
+    entry_list = CSV.read(joinpath(SITE_DIR, "temp",
+                                   "nut-free-mapper - Form Responses 1.csv"), DataFrame)
+    # @info entry_list
+    generate_index(entry_list; overwrite_existing=true)
+
     @info "Complete"
     return nothing
 end
